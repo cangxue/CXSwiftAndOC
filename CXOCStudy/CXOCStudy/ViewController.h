41ffdca8b96e87173964d3e2856e6ecebe655320
cangxue <752787086@qq.com>
2017-04-12 17:10:15 +0800

    build OCStudy project

---
+++ CXOCStudy/CXOCStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXOCStudy
+//
+//  Created by xiaoma on 17/4/12.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CXOCStudy/CXOCStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CXOCStudy
+//
+//  Created by xiaoma on 17/4/12.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
