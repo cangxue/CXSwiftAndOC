0e540ab146799220da48c498e25186b62e668335
cangxue <752787086@qq.com>
2017-08-15 11:30:30 +0800

    使用masonry进行自动布局

--- CXOCStudy/CXOCStudy/Classes/自动布局/CXAutoLayoutViewController.m
+++ CXOCStudy/CXOCStudy/Classes/自动布局/CXAutoLayoutViewController.m
@@ -20,7 +20,7 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    [self setNSlayoutConstraintsView];
+    [self setMasnoryConstraintView];
     
 }
 
@@ -77,6 +77,66 @@
 
 //使用masonry实现自动布局
 - (void)setMasnoryConstraintView {
+    
+    UIView *superview = self.view;
+    
+    UIView *view1 = [[UIView alloc] init];
+    view1.backgroundColor = [UIColor greenColor];
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor blueColor];
+    
+    UIView *view3 = [[UIView alloc] init];
+    view3.backgroundColor = [UIColor orangeColor];
+    
+    UIView *view4 = [[UIView alloc] init];
+    view4.backgroundColor = [UIColor redColor];
+    
+    [superview addSubview:view1];
+    
+    [superview addSubview:view2];
+    
+    [superview addSubview:view3];
+    
+    [superview addSubview:view4];
+    
+    UIEdgeInsets padding = UIEdgeInsetsMake(64 + 10, 10, 10, 10);
+
+    
+    [view1 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(superview.mas_top).with.offset(padding.top);
+//        make.left.equalTo(superview.mas_left).with.offset(padding.left);
+//        make.bottom.equalTo(superview.mas_bottom).with.offset(-padding.bottom);
+//        make.right.equalTo(superview.mas_right).with.offset(-padding.right);
+        
+        make.edges.equalTo(superview).with.insets(padding);
+        
+    }];
+    
+    
+    [view2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(view1.mas_top).with.offset(padding.left);
+        make.leading.equalTo(view1.mas_leading).with.offset(padding.left);
+        make.width.equalTo(view3);
+        make.height.equalTo(@100);
+    }];
+    
+    
+    [view3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(view2.mas_right).with.offset(padding.left);
+        make.trailing.equalTo(view1.mas_trailing).with.offset(-padding.left);
+        make.height.equalTo(view2);
+        make.width.equalTo(view2);
+        make.centerY.equalTo(view2.mas_centerY);
+    }];
+    
+    [view4 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(view3.mas_bottom).with.offset(padding.bottom);
+        make.leading.equalTo(view2.mas_leading);
+        make.trailing.equalTo(view3.mas_trailing);
+        make.height.equalTo(view3.mas_height);
+    }];
+    
     
 }
 
