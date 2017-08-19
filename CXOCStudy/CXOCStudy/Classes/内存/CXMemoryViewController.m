91be1a9d2eebb4dbfde657a9464b8e58dc55adf2
cangxue <752787086@qq.com>
2017-08-19 22:03:47 +0800

    初始化对象

--- CXOCStudy/CXOCStudy/Classes/内存/CXMemoryViewController.m
+++ CXOCStudy/CXOCStudy/Classes/内存/CXMemoryViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "CXMemoryViewController.h"
+#import "CXPerson.h"
 
 @interface CXMemoryViewController ()
+
+@property (nonatomic, copy) NSMutableArray *mainMutableArray;
 
 @end
 
@@ -16,6 +19,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+}
+
+- (void)buildObjectMethod {
+    /***  自己生成，自己持有   ***/
+    // 自己生成并持有对象
+    CXPerson *person1 = [CXPerson new];
+    
+    // 自己生成并持有对象
+    CXPerson *person2 = [[CXPerson alloc] init];
+    
+    // 基于NSCopying 由类实现copyWithZone: 方法生成并持有对象的副本
+    // 不可变更的对象
+    CXPerson *person3 = [person1 copy];
+    
+    // 基于NSMutableCopying 由类实现mutableCopyWithZone: 方法生成并持有对象的副本
+    // 可变更的对象
+    CXPerson *person4 = [person2 mutableCopy];
+    
+    
+    
+     /***  非自己生成的对象，自己持有   ***/
+    /*
+     - (instancetype)retain OBJC_ARC_UNAVAILABLE;
+     - (instancetype)autorelease OBJC_ARC_UNAVAILABLE;
+     */
+    NSMutableArray *array1 = [NSMutableArray array];
+    
+    
+//    NSLog(@"retain count = %lu", (unsigned long)[person2 retainCount]);
+    
     
 }
 
