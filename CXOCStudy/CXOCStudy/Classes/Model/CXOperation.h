47894a07eb855df162cd9d700190c381d104e209
cangxue <752787086@qq.com>
2017-08-31 11:05:22 +0800

    自定义operation类

---
+++ CXOCStudy/CXOCStudy/Classes/Model/CXOperation.h
@@ -0,0 +1,13 @@
+//
+//  CXOperation.h
+//  CXOCStudy
+//
+//  Created by xiaoma on 2017/8/30.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CXOperation : NSOperation
+
+@end
---
+++ CXOCStudy/CXOCStudy/Classes/Model/CXOperation.m
@@ -0,0 +1,13 @@
+//
+//  CXOperation.m
+//  CXOCStudy
+//
+//  Created by xiaoma on 2017/8/30.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import "CXOperation.h"
+
+@implementation CXOperation
+
+@end
