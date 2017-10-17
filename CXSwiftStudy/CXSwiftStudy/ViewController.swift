9eceb252a4da913e85525aeb0a95d92769da8456
cangxue <752787086@qq.com>
2017-10-17 14:40:17 +0800

    首页调整

--- CXSwiftStudy/CXSwiftStudy/ViewController.swift
+++ CXSwiftStudy/CXSwiftStudy/ViewController.swift
@@ -10,18 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var mainTableView: UITableView!
     
+    lazy var showArray = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.showArray = ["basic"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
     @IBAction func collectionBtnClick(_ sender: Any) {
         let vc = CollectionViewController()
@@ -40,7 +43,37 @@ class ViewController: UIViewController {
         vc.title = "Location"
         self.navigationController?.pushViewController(vc, animated: true)
     }
-
-
 }
 
+// MARK: - 代理方法
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.showArray.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "cellID"
+        let cell:UITableViewCell = UITableViewCell.init(style: .default, reuseIdentifier: cellId)
+        cell.selectionStyle = .none
+        cell.textLabel?.text = self.showArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var vc = UIViewController()
+        switch indexPath.row {
+        case 0:
+            vc = CollectionViewController()
+            
+        default:
+            vc = UIViewController()
+        }
+        
+        vc.title = self.showArray[indexPath.row]
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
