5eea5808b028242c71c7e070da1bfeb1c737ef9b
cangxue <752787086@qq.com>
2017-10-19 17:18:16 +0800

    测试

--- CXSwiftStudy/CXSwiftStudy/Classes/basic/BasicViewController.swift
+++ CXSwiftStudy/CXSwiftStudy/Classes/basic/BasicViewController.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class BasicViewController: UIViewController {
+    
+    let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.white
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        
+        var reversedNames = names.sorted(by: backward)
+        if reversedNames.count > 0 {
+            
+        }
         
     }
 
+    func backward(_ s1: String, _ s2: String) -> Bool {
+        return s1 > s2
+    }
+    
+    
 }
