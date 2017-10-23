d8fa91b6d3ad7e64325a9bab792b41514657ff33
cangxue <752787086@qq.com>
2017-10-23 14:02:48 +0800

    文件管理VC创建

---
+++ CXOCStudy/CXOCStudy/Classes/FileManager/CXFileManagerViewController.h
@@ -0,0 +1,14 @@
+//
+//  CXFileManagerViewController.h
+//  CXOCStudy
+//
+//  Created by xiaoma on 2017/10/23.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CXBaseViewController.h"
+
+@interface CXFileManagerViewController : CXBaseViewController
+
+@end
---
+++ CXOCStudy/CXOCStudy/Classes/FileManager/CXFileManagerViewController.m
@@ -0,0 +1,37 @@
+//
+//  CXFileManagerViewController.m
+//  CXOCStudy
+//
+//  Created by xiaoma on 2017/10/23.
+//  Copyright © 2017年 CX. All rights reserved.
+//
+
+#import "CXFileManagerViewController.h"
+
+@interface CXFileManagerViewController ()
+
+@end
+
+@implementation CXFileManagerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
