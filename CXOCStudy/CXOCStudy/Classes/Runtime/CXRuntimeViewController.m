dcd6e227462958d76c2e4c71a3a3e44526406e58
cangxue <752787086@qq.com>
2017-12-23 12:04:07 +0800

    runtime 基础方法使用

--- CXOCStudy/CXOCStudy/Classes/Runtime/CXRuntimeViewController.m
+++ CXOCStudy/CXOCStudy/Classes/Runtime/CXRuntimeViewController.m
@@ -28,36 +28,103 @@
 
 #pragma mark - 常用方法
 - (void)common_method {
-    /// 获取属性列表
-    unsigned int count;
-    objc_property_t *propertList = class_copyPropertyList([CXPerson class], &count);
-    for (unsigned int i = 0; i < count; i++) {
-        const char *propertyName = property_getName(propertList[i]);
-        NSLog(@"property -----> %@", [NSString stringWithUTF8String:propertyName]);
-    }
+    unsigned int outCount = 0;
     
-    /// 获取方法列表
-    Method *methodList = class_copyMethodList([CXPerson class], &count);
-    for (unsigned int i = 0; i < count; i++) {
-        Method method = methodList[i];
-        NSLog(@"mthod -----> %@", NSStringFromSelector(method_getName(method)));
-    }
+    Class selfClass = [self class];
+    Class testClass = [CXClass class];
+    
+    NSLog(@"===================== 基本信息 =======================");
+    
+    /// 获取类的类名
+    NSLog(@"class name -----> %s", class_getName(testClass));
+    
+    /// 获取父类
+    NSLog(@"super class -----> %s", class_getName(class_getSuperclass(testClass)));
+    
+    /// 是否是元类
+    NSLog(@"CXClass is%@ a meta-class", class_isMetaClass(testClass) ? @"" : @" not");
+    
+    /// 获取元类
+    Class meta_class = objc_getMetaClass(class_getName(testClass));
+    NSLog(@"%s meta-class is %s", class_getName(testClass), class_getName(meta_class));
+    
+    /// 获取实例大小
+    NSLog(@"instance size: %zu", class_getInstanceSize(testClass));
+    
+    NSLog(@"===================== 成员变量 =======================");
     
     /// 获取成员变量列表
-    Ivar *ivarList = class_copyIvarList([CXPerson class], &count);
-    for (unsigned int i = 0; i < count; i++) {
+    Ivar *ivarList = class_copyIvarList(testClass, &outCount);
+    for (int i = 0; i < outCount; i++) {
         Ivar ivar = ivarList[i];
-        const char *ivarName = ivar_getName(ivar);
-        NSLog(@"ivar -----> %@", [NSString stringWithUTF8String:ivarName]);
+        NSLog(@"instance variable name: %s at index: %d", ivar_getName(ivar), i);
     }
     
-    /// 获取协议列表
-    __unsafe_unretained Protocol **protocolList = class_copyProtocolList([self class], &count);
-    for (unsigned int i = 0; i < count; i++) {
-        Protocol *protocol = protocolList[i];
-        const char *protocolName = protocol_getName(protocol);
-        NSLog(@"Protocol -----> %@", [NSString stringWithUTF8String:protocolName]);
+    free(ivarList);
+    
+    /// 获取某个成员变量
+    Ivar string = class_getInstanceVariable(testClass, "_string");
+    if (string != NULL) {
+        NSLog(@"instance variable %s", ivar_getName(string));
     }
+    
+    NSLog(@"===================== 属性操作 =======================");
+    
+    /// 获取属性列表
+    objc_property_t *propertList = class_copyPropertyList(testClass, &outCount);
+    for (int i = 0; i < outCount; i++) {
+        objc_property_t property = propertList[i];
+        NSLog(@"property name -----> %s", property_getName(property));
+    }
+    
+    free(propertList);
+    /// 获取某个属性
+    objc_property_t array = class_getProperty(testClass, "array");
+    if (array != NULL) {
+        NSLog(@"property %s", property_getName(array));
+    }
+    
+    NSLog(@"===================== 方法操作 =======================");
+    
+    /// 获取所有方法列表
+    Method *methodList = class_copyMethodList(testClass, &outCount);
+    for (int i = 0; i < outCount; i++) {
+        Method method = methodList[i];
+        NSLog(@"mthod signature: -----> %@", NSStringFromSelector(method_getName(method)));
+    }
+    
+    free(methodList);
+    
+    /// 获取实例方法
+    Method method1 = class_getInstanceMethod(testClass, @selector(method1));
+    if (method1 != NULL) {
+        NSLog(@"instance method ------> %@", NSStringFromSelector(method_getName(method1)));
+    }
+    
+    /// 获得类方法
+    Method classMethod = class_getClassMethod(testClass, @selector(classMethod1));
+    if (classMethod != NULL) {
+        NSLog(@"class method ------> %@", NSStringFromSelector(method_getName(classMethod)));
+    }
+    
+    NSLog(@"CXClass is%@ responsd to selector: method3WithArg1:arg2:", class_respondsToSelector(testClass, @selector(method3WithArg1:arg2:)) ? @"" : @" not");
+    
+    /// 方法实现
+    IMP imp = class_getMethodImplementation(testClass, @selector(method1));
+    imp(self, @selector(method1));
+    
+    NSLog(@"===================== 协议 =======================");
+    
+    /// 获取协议列表
+    //    Protocol * __unsafe_unretained * protocols = class_copyProtocolList(testClass, &outCount);
+    __unsafe_unretained Protocol **protocolList = class_copyProtocolList(testClass, &outCount);
+    for (unsigned int i = 0; i < outCount; i++) {
+        Protocol *protocol = protocolList[i];
+        NSLog(@"protocol name: -----> %s", protocol_getName(protocol));
+    }
+    
+    Protocol *protocol1 = protocolList[0];
+    NSLog(@"CXClass is%@ responsed to protocol %s", class_conformsToProtocol(testClass, protocol1) ? @"" : @"not", protocol_getName(protocol1));
 }
 
 
