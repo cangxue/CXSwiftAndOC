e2a63e07421532579af435fcb581e21ea56ac558
cangxue <752787086@qq.com>
2018-03-08 16:04:43 +0800

    swift单例实现方式

--- CXSwiftStudy/CXSwiftStudy/design-mode/CXSingleton.swift
+++ CXSwiftStudy/CXSwiftStudy/design-mode/CXSingleton.swift
@@ -20,3 +20,70 @@ class CXSingleton: NSObject {
     }
     
 }
+
+// 1、直接访问静态属性的方式
+// 不能被继承
+final class Singleton1: NSObject {
+    // 静态变量
+    static let shareInstance = Singleton1()
+    // 私有变量，类外无法访问
+    private override init() {
+        print("Singleton 初始化了一次")
+        
+    }
+}
+
+//2、带立即执行的闭包初始化器的全局变量
+final class Singleton2: NSObject {
+    
+    static var shareInstance: Singleton2 = {
+        let instance = Singleton2()
+        return instance
+    }()
+    
+    private override init(){
+        print("Singleton2 初始化了一次")
+        
+    }
+}
+
+//3、结构体实现单例
+final class Singleton3: NSObject {
+    private class var shared : Singleton3 {
+        struct SharedSingleton {
+            static let instance = Singleton3()
+            
+        }
+        
+        return SharedSingleton.instance
+        
+    }
+    
+    class func shareInstance() -> Singleton3 {
+        return shared
+        
+    }
+    
+    private override init() {
+        print("Singleton3 初始化了一次")
+        
+    }
+    
+}
+
+//4、定义私有静态变量 通过公有方法访问
+final class Singleton4: NSObject {
+    
+    private static let instance = Singleton4()
+    
+    class func shareInstance() -> Singleton4 {
+        return instance
+        
+    }
+    
+    private override init() {
+        print("Singleton4 初始化了一次")
+        
+    }
+    
+}
